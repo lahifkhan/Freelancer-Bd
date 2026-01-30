32f06a2d9358553c8750d3595ce10877c72aab4f
lahifkhan <lahifkhan52@gmail.com>
2026-01-30 07:52:07 +0600

    server live site added

--- src/Hook/useAxiosSecure.jsx
+++ src/Hook/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://freelencer-server.vercel.app",
 });
 
 const useAxiosSecure = () => {
